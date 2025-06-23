a75607604d56ba26fc233a10fdca2835ce5d4e8c
Anesu C.J Sibanda <87155971+cjsibanda@users.noreply.github.com>
2025-06-23 10:03:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ $('body').scrollspy({
     target: '.navbar-fixed-top'
 })
 
-// Closes the Responsive Menu on Menu Item Click
+// Closes the Responsive Menu on click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
